5397ff6f689f2256a441f27b4ec1ae26f7fb3c13
honest1320 <63172797+honest1320@users.noreply.github.com>
2020-10-15 17:28:44 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,6 @@
-﻿/*******************************************************************************************************************************
-**				                                    .........HONEST TEMU......:     
-                
-                Basıt Açıklama:
-  				      Bir atık uygun kutuya atıldığında, atığın hacmi kadar puan kazanılacak ve hemen rastgele yeni bir atık
-                üretilecektir.
-                Bir atık kutusunda, eklenmek istenen atığı alacak kadar boş yer yoksa atık kutuya eklenmeyecektir. Bu
-                durumda atığı kutuya ekleyebilmek için önce kutunun boşaltılması gerekecektir.
-                Bir atık kutusunun boşaltılabilmesi için en az %75 dolu olması gerekmektedir. Dolu hacim %75’in
-                altındaysa “Boşalt” düğmesine basıldığında hiçbir işlem yapılmayacaktır.                           
-****************************************************************************************************************************/
+﻿/***********************************************************************************
+**				               .........HONEST TEMU......:     
+*************************************************************************************/
 
 
 using System;
