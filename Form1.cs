8ffb0580b4d205937352cd513fccdb7e544e0b0b
honest1320 <63172797+honest1320@users.noreply.github.com>
2020-10-15 17:23:21 +0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,14 @@
-﻿/****************************************************************************
-**					      SAKARYA ÜNİVERSİTESİ                             
-**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ                  
-**				     BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ                        
-**				    NESNEYE DAYALI PROGRAMLAMA DERSİ                       
-**					     2019-2020 BAHAR DÖNEMİ                            
-**	                                                                      
-**				ÖDEV NUMARASI.......1. Proje / Tasarım...:                              
-**				ÖĞRENCİ ADI.........HONEST TEMU......:                             
-**				ÖĞRENCİ NUMARASI....B181210561...:                            
-**              DERSİN ALINDIĞI GRUP..........1A........:                               
-****************************************************************************/
+﻿/*******************************************************************************************************************************
+**				ADI.........HONEST TEMU......:     
+                
+                Basıt Açıklama:
+**				Bir atık uygun kutuya atıldığında, atığın hacmi kadar puan kazanılacak ve hemen rastgele yeni bir atık
+                üretilecektir.
+                Bir atık kutusunda, eklenmek istenen atığı alacak kadar boş yer yoksa atık kutuya eklenmeyecektir. Bu
+                durumda atığı kutuya ekleyebilmek için önce kutunun boşaltılması gerekecektir.
+                Bir atık kutusunun boşaltılabilmesi için en az %75 dolu olması gerekmektedir. Dolu hacim %75’in
+                altındaysa “Boşalt” düğmesine basıldığında hiçbir işlem yapılmayacaktır.                           
+****************************************************************************************************************************/
 
 
 using System;
